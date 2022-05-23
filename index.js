425429371c6877f86faec2436f46f3bb7e60c04b
Saykot43 <saykot.cse43@gmail.com>
2022-05-23 09:48:07 +0600

    product get done

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 const express = require('express')
-require('dotenv').config()
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 const app = express()
 const cors = require('cors')
 app.use(cors())
+app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c5gx8.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
         await client.connect();
-        const database = client.db("sample_mflix");
-        const movies = database.collection("movies");
-        // Query for a movie that has the title 'The Room'
-        const query = { title: "The Room" };
-        const options = {
-            // sort matched documents in descending order by rating
-            sort: { "imdb.rating": -1 },
-            // Include only the `title` and `imdb` fields in the returned document
-            projection: { _id: 0, title: 1, imdb: 1 },
-        };
-        const movie = await movies.findOne(query, options);
-        // since this method returns the matched document, not a cursor, print it directly
-        console.log(movie);
+        const productCollections = client.db('mobile-manufacture').collection("products")
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const cursor = productCollections.find(query);
+            const result = await cursor.toArray()
+            res.send(result);
+        })
+
     } finally {
-        await client.close();
+
     }
 }
 run().catch(console.dir);
